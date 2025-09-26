580facb8d88a47e9545ef4c109c02d550324eb25
AdisonJ555 <kaneki.adison@gmail.com>
2025-09-25 21:55:56 -0300

    criacao-feed

--- Senai-notes-angular/src/app/app.routes.ts
+++ Senai-notes-angular/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { LoginScreen } from './user-module/login-screen/login-screen';
 import { CadastroScreen } from './user-module/cadastro-screen/cadastro-screen';
+import { FeedScreen } from './feed-screen/feed-screen';
 
 export const routes: Routes = [
     {
@@ -11,7 +12,9 @@ export const routes: Routes = [
         path: "cadastro",
         loadComponent: () => CadastroScreen
 
+    }, 
+    {
+        path: "feed",
+        loadComponent: () => FeedScreen
     }
-
-
 ];
